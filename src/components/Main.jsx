baa202105a389176af7c9d649729df96c9f09f91
TheHighnote <ryanhighnote7@gmail.com>
2023-03-28 14:33:49 -0500

    bug fix from about route

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navbar, Home, About, Locations } from "./";
+import { Navbar, Home, Locations } from "./";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Profile, Footer } from "./index";
 import AllVehicles from "./AllVehicles";
 import Login from "./Login";
 import Register from "./Register";
+import About from "./About";
 
 
 //this is to start a branch
