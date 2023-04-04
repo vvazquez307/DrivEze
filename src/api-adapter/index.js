968eaa9d424fac432b0757ad91bed7c260fa30ec
Logan <logancable@ymail.com>
2023-04-04 11:02:26 -0700

    added all vehicles list and searchBar

--- src/api-adapter/index.js
+++ src/api-adapter/index.js
@@ -1,1 +1,14 @@
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "http://localhost:3000/api";
+
+export const getAllVehicles = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/cars`, {
+      method: "GET",
+    });
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/AllVehicles.jsx
+++ src/components/AllVehicles.jsx
@@ -1,40 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getAllVehicles } from "../api-adapter";
 
 function AllVehicles() {
+  const [vehicles, setVehicles] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searched, setSearched] = useState("");
+
+  useEffect(() => {
+    async function allVehicles() {
+      let vehicles = await getAllVehicles();
+      setVehicles(vehicles);
+    }
+    allVehicles();
+  }, []);
+
+  const searchedVehicle = vehicles.filter((vehicle) => {
+    return vehicle.name.toLowerCase().startsWith(searched.toLowerCase());
+  });
+
+  const searchHandle = (e) => {
+    setSearchTerm(e.target.value);
+    setSearched(e.target.value);
+  };
   return (
     <>
       <div className="allVehiclesPage">
         <div className="allVehiclesTopDiv">
-          <div className="searchBar">search bar</div>
+          <div className="searchBarDiv">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={searchHandle}
+              className="searchBar"
+            />
+          </div>
           <div className="filters">filters </div>
         </div>
         <div className="allVehiclesBottomDiv">
-          <div className="vehicleListing">
-            <div className="vehicleImg">img</div>
-            <div className="vehicleDescription">description</div>
-            <div className="addToCart">add to cart</div>
-          </div>
-          <div className="vehicleListing">
-            <div className="vehicleImg">img</div>
-            <div className="vehicleDescription">description</div>
-            <div className="addToCart">add to cart</div>
-          </div>
-          <div className="vehicleListing">
-            <div className="vehicleImg">img</div>
-            <div className="vehicleDescription">description</div>
-            <div className="addToCart">add to cart</div>
-          </div>
-          <div className="vehicleListing">
-            <div className="vehicleImg">img</div>
-            <div className="vehicleDescription">description</div>
-            <div className="addToCart">add to cart</div>
-          </div>
+          {searchedVehicle.map((vehicle) => (
+            <div className="vehicleListing" key={vehicle.id}>
+              <div className="vehicleImg">
+                img
+                <div className="vehicleName">{vehicle.name}</div>
+              </div>
+              <div className="vehicleDescription">{vehicle.description}</div>
+              <div className="addToCart">
+                <img
+                  src="/images/Cart.png"
+                  alt="cartImage"
+                  className="allVehiclesCart"
+                />
+                <div>add to cart</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <Link to="/">Go Back</Link>
     </>
   );
 }
-
 export default AllVehicles;
