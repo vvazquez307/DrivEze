dbd51a14e66108fc0cf7ee3559a0a1a7e96ae4a5
TheHighnote <ryanhighnote7@gmail.com>
2023-04-04 13:22:05 -0500

    fixed merge error

--- src/api-adapter/index.js
+++ src/api-adapter/index.js
@@ -23,13 +23,20 @@ export const createCart = async (userId) => {
 };
 
 export const getCartByUserId = async (userId) => {
-  try {
-    const response = await fetch(`${BASE_URL}/cart/${userId}`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const result = await response.json();
+    try {
+      const response = await fetch(`${BASE_URL}/cart/${userId}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+  
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
 
 export const addCarToCart = async (userId, carId, price) => {
   try {
