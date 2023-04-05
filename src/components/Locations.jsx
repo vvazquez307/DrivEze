e5ccff77605067f3ce877420bcc95f8afc921cc1
TheHighnote <ryanhighnote7@gmail.com>
2023-04-05 10:38:22 -0500

    Hub Locations now display in a list, added CSS as well

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,15 +1,37 @@
-import React from "react";
 
-const Locations = () => {
-  return (
-    <div>
-      <ol>Locations!</ol>
-      <li>Denver, Colorado</li>
-      <li>Houston, Texas</li>
-      <li>Pheonix, Arizona</li>
-      <li>Las Vegas, Nevada</li>
-    </div>
-  );
-};
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { getAllHubs } from "../api-adapter/hub";
 
-export default Locations;
+function AllHubs(){
+  const [locations, setLocations] = useState([])
+
+
+useEffect(() => {
+  async function allHubs() {
+    let hubs = await getAllHubs();
+    setLocations(hubs);
+  }
+  allHubs();
+}, []);
+
+
+return (
+  <>
+  <div id="hubs">
+    <h1 id="hub-title-text">Hub Locations</h1>
+    
+  {locations.map(location => (
+  <div id="hub-view" key={location.id}>
+            
+            <h3 id="locations-list">{location.location}</h3>
+            </div>
+    ))}
+      <Link to="/" id="back-button">Go Back</Link>
+      </div>
+    </>
+)
+}
+
+
+export default AllHubs;
