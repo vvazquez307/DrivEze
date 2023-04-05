caad259454f9e729f058b46c83ed569193271f33
Logan <logancable@ymail.com>
2023-04-05 10:07:55 -0700

    on login fetches cart assigned to user

--- src/api-adapter/login&register.js
+++ src/api-adapter/login&register.js
@@ -30,21 +30,21 @@ export const loginUser = async (username, password) => {
   } catch (error) {}
 };
 
-export const assignCart = async (userId) => {
-  try {
-    const response = await fetch(`${BASE_URL}/cart/new`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userId }),
-    });
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// export const assignCart = async (userId) => {
+//   try {
+//     const response = await fetch(`${BASE_URL}/cart/new`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ userId }),
+//     });
+//     const result = await response.json();
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 export const getCart = async (token) => {
   try {
