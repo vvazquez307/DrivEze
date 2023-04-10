f9a2bb73de8b8cf7b8f1f4095230169f8567746d
victor <vvazquez307@gmail.com>
2023-04-10 13:51:18 -0500

    fixed a couple of things

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Checkout() {
   const [name, setName] = useState("");
   const [ccNumber, setCCNumber] = useState("");
   const [securityNumber, setSecurityNumber] = useState("");
   const [paymentStatus, setPaymentStatus] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const totalCost = location.state.totalSum;
+
+  console.log(totalCost);
 
   function checkName() {
     if (name.length < 6) {
@@ -38,72 +45,86 @@ function Checkout() {
       return false;
     }
   }
+  function takeMeHome() {
+    setTimeout(() => {
+      navigate("/");
+    }, 2500);
+  }
 
   useEffect(() => {
     checkAllInputs();
   }, [name, ccNumber, securityNumber]);
   return (
-    <div id="checkout-container">
-      <h1>Payment Proccess</h1>
-      <div id="credit-card-name-input">
-        <label>Name: </label>
-        <input
-          type="text"
-          name="credit-card-name"
-          placeholder="John Wick"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <div id="credit-card-#-input">
-        <label>Credit card #: </label>
-        <input
-          type="text"
-          pattern="[0-9]{16}"
-          maxlength="16"
-          name="credit-card-number"
-          placeholder="1111-2222-3333-4444"
-          value={ccNumber}
-          onChange={(event) => setCCNumber(event.target.value)}
-        />
-      </div>
-      <div id="credit-card-security-input">
-        <label>security # </label>
-        <input
-          maxlength="3"
-          type="number"
-          name="credit-card-security"
-          placeholder="123"
-          value={securityNumber}
-          onChange={(event) => {
-            let currentValue = event.target.value;
-            let currentValueStr = String(currentValue);
-            if (currentValueStr.length > 3) {
-              return;
-            } else {
-              setSecurityNumber(currentValue);
-            }
-          }}
-        />
-      </div>
-      <h1>Order Summary</h1>
-      <h3>Total: $100</h3>
-      {checkAllInputs() ? (
+    <>
+      {paymentStatus ? (
         <div>
-          <button
-            onClick={() => {
-              setPaymentStatus(true);
-            }}
-            id="order-button-green"
-          >
-            Complete Order
-          </button>
+          {takeMeHome()}
+          <h1>Thank You For Your Purchase!</h1>
+          <p>you are now being redirected to the main page...</p>
         </div>
       ) : (
-        <button id="order-button-red">Complete Order</button>
+        <div id="checkout-container">
+          <h1>Payment Proccess</h1>
+          <div id="credit-card-name-input">
+            <label>Name: </label>
+            <input
+              type="text"
+              name="credit-card-name"
+              placeholder="John Wick"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div id="credit-card-#-input">
+            <label>Credit card #: </label>
+            <input
+              type="text"
+              pattern="[0-9]{16}"
+              maxlength="16"
+              name="credit-card-number"
+              placeholder="1111-2222-3333-4444"
+              value={ccNumber}
+              onChange={(event) => setCCNumber(event.target.value)}
+            />
+          </div>
+          <div id="credit-card-security-input">
+            <label>security # </label>
+            <input
+              maxlength="3"
+              type="number"
+              name="credit-card-security"
+              placeholder="123"
+              value={securityNumber}
+              onChange={(event) => {
+                let currentValue = event.target.value;
+                let currentValueStr = String(currentValue);
+                if (currentValueStr.length > 3) {
+                  return;
+                } else {
+                  setSecurityNumber(currentValue);
+                }
+              }}
+            />
+          </div>
+          <h1>Order Summary</h1>
+          <h3>Total: ${totalCost}</h3>
+          {checkAllInputs() ? (
+            <div>
+              <button
+                onClick={() => {
+                  setPaymentStatus(true);
+                }}
+                id="order-button-green"
+              >
+                Complete Order
+              </button>
+            </div>
+          ) : (
+            <button id="order-button-red">Complete Order</button>
+          )}
+        </div>
       )}
-      {paymentStatus ? <h1>Thank You For Your Purchase!</h1> : null}
-    </div>
+    </>
   );
 }
 
