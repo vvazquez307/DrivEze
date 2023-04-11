ad6e23b73e9718ffe9860a6825f7aa05239adc3e
UpcomingWhale <123348874+JavaScriptCodingGuru@users.noreply.github.com>
2023-04-11 15:08:51 -0400

    inventory, locations

--- src/components/AdminInventory.jsx
+++ src/components/AdminInventory.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAllHubs } from "../api-adapter/hub";
+import { getAllHubs, hubInventory } from "../api-adapter/hub";
+import { getAllVehicles } from "../api-adapter";
 
 
 
@@ -10,24 +11,56 @@ function AdminInventory(props)
     const isAdmin = props.isAdmin;
 
     const [hubs, setHubs] = useState([]);
-
+    const [hubInventories, setHubInventories] = useState([]);
+    const [cars, setCars] = useState([]);
 
     useEffect(()=>
     {
         async function GetHubs()
         {
             const hub = await getAllHubs();
+            const car = await getAllVehicles();
+            const hubInventories = [];
+            for(let i = 0; i<hub.length; i++)
+            {
+                hubInventories.push(await hubInventory(hub[i].id));
+            }
+            for(let i = 0; i < hubInventories.length; i++)
+            {
+                for(let j = 0; i < cars.length; j++)
+                {
+
+                } 
+            }
+            console.log(hubInventories)
             setHubs(hub);
+            setHubInventories(hubInventories);
         }
-    })
+        GetHubs();
+    }, [])
 
     function ShowData()
     {
 
+        console.log(hubs);
+        
+        
 
         return (
             <div>
-                
+                {
+                    hubs.map((hub, index)=>
+                    {
+                        return(
+                            <div>
+                            <h3>{hub.location}</h3>
+                            {
+                                hubInventories[index][0].name
+                            }
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
