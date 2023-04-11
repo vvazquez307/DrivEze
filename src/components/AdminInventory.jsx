790a2a38412cef313f7229c0f140e60ac3fcc578
UpcomingWhale <123348874+JavaScriptCodingGuru@users.noreply.github.com>
2023-04-11 13:22:55 -0400

    admin inventory

--- src/components/AdminInventory.jsx
+++ src/components/AdminInventory.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getAllHubs } from "../api-adapter/hub";
 
 
 
@@ -8,11 +9,33 @@ function AdminInventory(props)
     const navigate = useNavigate();
     const isAdmin = props.isAdmin;
 
+    const [hubs, setHubs] = useState([]);
+
+
+    useEffect(()=>
+    {
+        async function GetHubs()
+        {
+            const hub = await getAllHubs();
+            setHubs(hub);
+        }
+    })
+
+    function ShowData()
+    {
+
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 
     return(
         <div>
             {
-                isAdmin? <h1>Admin Inventory Page</h1> : navigate("/adminLogin")
+                isAdmin? <div><h1>Admin Inventory Page</h1> <ShowData/></div>: navigate("/adminLogin")
             }
         </div>
     )
